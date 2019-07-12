c3aef8ca5ed46eb65edb45e552ca387a19500c9a
Paulito <Paulito123@users.noreply.github.com>
2019-07-12 16:19:20 +0200

    Initial check-in TestForm

---
+++ BitMEX.TestForm/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace BitMEX.TestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnMarketOrder = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.TBMarketOrder = new System.Windows.Forms.TextBox();
+            this.NUDMarketOrderQuantity = new System.Windows.Forms.NumericUpDown();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDMarketOrderQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 8;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Controls.Add(this.btnMarketOrder, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.TBMarketOrder, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.NUDMarketOrderQuantity, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.OutputLabel, 0, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 426);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btnMarketOrder
+            // 
+            this.btnMarketOrder.Location = new System.Drawing.Point(3, 3);
+            this.btnMarketOrder.Name = "btnMarketOrder";
+            this.btnMarketOrder.Size = new System.Drawing.Size(75, 23);
+            this.btnMarketOrder.TabIndex = 0;
+            this.btnMarketOrder.Text = "MarketOrder";
+            this.btnMarketOrder.UseVisualStyleBackColor = true;
+            this.btnMarketOrder.Click += new System.EventHandler(this.btnMarketOrder_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 129);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(3, 171);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // TBMarketOrder
+            // 
+            this.TBMarketOrder.Location = new System.Drawing.Point(100, 3);
+            this.TBMarketOrder.Name = "TBMarketOrder";
+            this.TBMarketOrder.Size = new System.Drawing.Size(91, 20);
+            this.TBMarketOrder.TabIndex = 7;
+            this.TBMarketOrder.Text = "XBTUSD";
+            // 
+            // NUDMarketOrderQuantity
+            // 
+            this.NUDMarketOrderQuantity.Location = new System.Drawing.Point(197, 3);
+            this.NUDMarketOrderQuantity.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.NUDMarketOrderQuantity.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            -2147483648});
+            this.NUDMarketOrderQuantity.Name = "NUDMarketOrderQuantity";
+            this.NUDMarketOrderQuantity.Size = new System.Drawing.Size(91, 20);
+            this.NUDMarketOrderQuantity.TabIndex = 8;
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(3, 210);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(52, 13);
+            this.OutputLabel.TabIndex = 9;
+            this.OutputLabel.Text = "OUTPUT";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NUDMarketOrderQuantity)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnMarketOrder;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox TBMarketOrder;
+        private System.Windows.Forms.NumericUpDown NUDMarketOrderQuantity;
+        private System.Windows.Forms.Label OutputLabel;
+    }
+}
+
---
+++ BitMEX.TestForm/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BitMEX;
+using BitMEX.JSONClass.Order;
+
+namespace BitMEX.TestForm
+{
+    public partial class Form1 : Form
+    {
+        private MordoR mconn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mconn = new MordoR();
+            TBMarketOrder.Text = "XBTUSD";
+        }
+
+        private void btnMarketOrder_Click(object sender, EventArgs e)
+        {
+            if (NUDMarketOrderQuantity.Value >= 1 || NUDMarketOrderQuantity.Value <= -1)
+            {
+                try
+                {
+                    string lastOrderID;
+                    object outcome = mconn.MarketOrder(TBMarketOrder.Text.ToString(), out lastOrderID, Decimal.ToInt32((decimal)NUDMarketOrderQuantity.Value));
+
+                    switch (outcome.GetType().ToString())
+                    {
+                        case "BitMEX.JSONClass.Order.OrderResponse":
+                            OutputLabel.Text = ((OrderResponse)outcome).ClOrdId.ToString();
+                            MessageBox.Show(((OrderResponse)outcome).ClOrdId.ToString());
+                            break;
+                        case "BitMEX.JSONClass.Order.OrderError":
+                            MessageBox.Show(((OrderError)outcome).Error.Message.ToString());
+                            break;
+                        default:
+                            MessageBox.Show("bla");
+                            break;
+                    }
+                }
+                catch (Exception exc)
+                {
+                    // Catch all external exceptions like connection issues etc.
+                    MessageBox.Show(exc.Message.ToString());
+                }
+            }
+        }
+    }
+}
